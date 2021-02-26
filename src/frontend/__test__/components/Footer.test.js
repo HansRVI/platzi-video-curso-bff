78ccf3ed810077f7c5bf2791ff9ce85f3aa02f29
hansrvi <hansrvi@gmail.com>
2021-02-26 00:35:58 -0600

    fin del proyecto despues de agregarle pruebas con jest

--- src/frontend/__test__/components/Footer.test.js
+++ src/frontend/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import {create} from "react-test-renderer"
 import Footer from '../../components/Footer';
 // describe crea algo que se llama una suit
 // las suit siven para poder utilizar mas de una prueba
@@ -15,5 +16,16 @@ describe('<Footer />', () => {
   test('Footer haves 3 anchors', () => {
     expect(footer.find('a')).toHaveLength(3);
   });
+   
+  test('Footer Snapshot', () => {
+     const footer = create(<Footer />);
+     expect(footer.toJSON()).toMatchSnapshot();
+ });
+  
 
 });
+
+// la primera vez que se corre la prueba expect(footer.toJSON()).toMatchSnapshot(); crea un snapshot del nodo completo
+// las siguientes veces comprar el snapshot creado con lo que tenga el nodo actualmente
+// si no coninciden falla la prueba
+// para actualizar el snapshot se corre el comando: jest --updateSnapshot
